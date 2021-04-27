b3474aa548b36b82b1707863d22625986374f912
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-04-27 14:55:42 +0200

    use babel + jsx

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 let n = 0
 
-function render(){
-    const title = React.createElement('h1', {}, 
-        'Bonjour tout le monde ',
-        React.createElement('span', {}, n)
-    )
+function numberFormat(n) {
+    return n.toString().padStart(2, '0')
+}
+
+function render() {
+    const title = <h1 className="title" id={"title" + n}>
+        Bonjour les gens <span>{n}</span>
+        </h1>
 
     ReactDOM.render(title, document.querySelector('#app'))
 }
