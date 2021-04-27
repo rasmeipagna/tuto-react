b4e6fc4958b0916beecb0ced971d5e9e615e1304
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-04-27 15:00:29 +0200

    add React.Fragment

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ function numberFormat(n) {
 }
 
 function render() {
-    const title = <h1 className="title" id={"title" + n}>
+    const items = [
+        'Tache 1',
+        'Tache 2',
+        'Tache 3'
+    ]
+    const title = <React.Fragment><h1 className="title" id="title">
         Bonjour les gens <span>{n}</span>
         </h1>
+        <ul>
+            <li>Premier li</li>
+        </ul>
+        </React.Fragment>
 
     ReactDOM.render(title, document.querySelector('#app'))
 }
