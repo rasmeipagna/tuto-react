c085cc2d5cf57b1aa8f2c02de9b5b56193def796
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-04-28 21:08:28 +0200

    add event and different methods

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function WelcomeFunc ({name, children}){
     return <div>
-    <h1>Bonjour {name}</h1>
+    <h1>Bonjour {name} avec un function</h1>
     <p>
         {children}
     </p>
@@ -11,7 +11,7 @@ class Welcome extends React.Component {
 
     render() {
         return <div>
-        <h1>Bonjour {this.props.name}</h1>
+        <h1>Bonjour {this.props.name} avec un class extends </h1>
             <p>
                 {this.props.children}
             </p>
@@ -53,21 +53,56 @@ class Clock extends React.Component {
 class Incrementer extends React.Component{
     constructor (props) {
         super(props)
-        this.state = {n: props.start}
-        this.timer = null
+        this.state = {n: props.start, timer: null}
+        this.toggle = this.toggle.bind(this)
+        this.reset = this.reset.bind(this)
     }
-    componentDidMount (){
-        window.setInterval(this.increment.bind(this), 1000)
+
+    componentDidMount () {
+        this.play()
     }
-    componentwillUnmount (){
-        window.clearInterval(this.timer)
+
+    componentwillUnmount () {
+        window.clearInterval(this.state.timer)
     }
+
     increment () {
         this.setState((state, props) => ({n: state.n + props.step}))
     }
 
-    render () {
-        return <div>Valeur : {this.state.n}</div>
+    pause () {
+        window.clearInterval(this.state.timer)
+        this.setState({
+            timer: null
+        })
+    }
+
+    play () {
+        window.clearInterval(this.state.timer)
+        this.setState({
+            timer: window.setInterval(this.increment.bind(this), 1000)
+        })
+    }
+    // au click permet de switch entre play/pause
+    toggle () {
+        return this.state.timer ? this.pause() : this.play()
+    }
+    // au click on change le label
+    label () {
+        return this.state.timer ? 'Pause' : 'Lecture'
+    }
+    reset () {
+        this.pause()
+        this.play()
+        this.setState((state, props) => ({n: props.start}))
+    }
+
+    render () {       
+        return <div>Valeur : {this.state.n}
+        
+            <button onClick={this.toggle}>{this.label()}</button> 
+            <button onClick={this.reset}>Réinitialiser</button>
+        </div>
     }
 }
 //Incremente de 10 par 10
@@ -75,11 +110,35 @@ Incrementer.defaultProps = {
     start:0,
     step:1
 }
+
+class ManualIncrementer extends React.Component {
+    constructor (props) {
+        super(props) 
+        this.state = {n: 0}
+    }
+
+    increment () {
+        this.setState((state, props) => ({n: state.n + 1}))
+    }
+
+
+    render () {
+        return <div> 
+            Valeur : {this.state.n} <button onClick={this.increment.bind(this)}>Incrémenter</button>
+
+
+        </div>
+    }
+}
 function Home () {
     return <div>
         <Welcome name="Pagna" />
         <Welcome name="Jean" />
         <Clock/>
+        {/* ici on créer un bouton pou qu'a chaque clic on incrémente */}
+        <ManualIncrementer />
+        {/* ici on incremente automatique 1 par 1 à partir de 1 */}
+        <Incrementer />
         {/* ici on incremente chaque seconde à partir de 10 */}
         <Incrementer start={10} />
         {/* ici on incremente de 10 par 10 */}
