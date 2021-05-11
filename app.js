bd415e68c959d00e9e232231d978afe0fc7c81bb
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-05-11 18:14:40 +0200

    add component convert celsius

--- app.js
+++ app.js
@@ -5,4 +5,24 @@ function BoilingVerdict({celsius}) {
     return <div className ="alert alert-info"> L'eau ne bout pas</div>
 }
 
+class Calculator extends React.Component{
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            temperature:''
+        }
+    }
+    render () {
+        const {temperature} = this.state
+        return <div>
+            <div className="form-group">
+                <label htmlFor="celsius">Temperature (en Celsius)</label>
+                <input type="text" id="celsius" value={temperature} class="form-control"></input>
+                <BoilingVerdict celsius={parseFloat(temperature)}/>
+            </div>
+        </div>
+    }
+}
+
 ReactDOM.render(<BoilingVerdict celsius={110}/>, document.getElementById('app'))
