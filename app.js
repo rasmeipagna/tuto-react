cc788f0804553e5fc336238dff056d8c94e752e2
RasmeiPagna <rasmeipagna.toung@gmail.com>
2021-05-11 19:22:02 +0200

    add celsius/farenheit covnert

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const scaleNames = {
+    c: 'Celsius',
+    f : 'Farenheit'
+}
 function BoilingVerdict({celsius}) {
     if (celsius >= 100) {
         return <div className="alert alert-success">L'eau bout</div>
@@ -5,23 +9,49 @@ function BoilingVerdict({celsius}) {
     return <div className ="alert alert-info"> L'eau ne bout pas</div>
 }
 
+class TemperatureInput extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {temperature : ''}
+        this.handleChange = this.handleChange.bind(this)
+
+    }
+    handleChange (e) {
+        this.setState({temperature : e.target.value})
+    }
+
+    render () {
+        const {temperature} = this.state
+        const name = 'scale' + this.props.scale
+        const scaleName = scaleNames[this.props.scale]
+        return <div className= "form-group">
+            <label htmlFor={name}> Température (en {scaleName})</label>
+            <input type="text" id={name} value={temperature} className="form-control" onChange={this.handleChange}></input>
+        </div>
+    }
+}
+
 class Calculator extends React.Component{
 
     constructor(props) {
         super(props)
         this.state = {
-            temperature:''
+            temperature: 20
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange (e) {
+        this.setState({temperature : e.target.value})
     }
     render () {
         const {temperature} = this.state
         return <div>
             <div className="form-group">
-                <label htmlFor="celsius">Temperature (en Celsius)</label>
-                <input type="text" id="celsius" value={temperature} className="form-control">
-                </input>
-            </div>
+                <TemperatureInput scale="c" temperature={temperature}/>
+                <TemperatureInput scale="f" temperature={}/>
                 <BoilingVerdict celsius={parseFloat(temperature)}/>
+            </div>
+                
            
         </div>
     }
