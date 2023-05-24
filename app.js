ca594f78b6836558349c277dda3fc80733201105
RasmeiPagna <rasmeipagna.toung@gmail.com>
2023-05-24 14:41:43 +0200

    suite 9-liste produit

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const PRODUCTS = [
 
   function ProductRow ({product}) {
     const name = product.stocked ?
-    product.name : <span className="text-danger">{product.name}</span>
+    product.name :
+    <span className="text-danger">{product.name}</span>
     return <tr>
         <td>{name}</td>
         <td>{product.price}</td>
